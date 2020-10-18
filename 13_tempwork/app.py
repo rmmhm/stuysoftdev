6a8b4e05523b6821cd7da7573b1f55ab1c334e42
Ryan Ma <justicar270@gmail.com>
2020-10-18 13:52:17 -0400

    finish flasky part of k13

--- 13_tempwork/app.py
+++ 13_tempwork/app.py
@@ -21,25 +21,31 @@ def occupy():
     data = occupations.readlines()
     occupations.close()
     current_total = 0
-    percent_dict = {}
+    occupations_dict = {}
     for i in range(1, len(data)-1):
         if data[i][0] == '"':
-            data_set = data[i].split('",')
-            title = data_set[0][1:]
+            data_set1 = data[i].split('",')
+            title = data_set1[0][1:]
+            data_set2 = data_set1[1].split(",") # splits the data set again because otherwise, the percent and website would be stuck together
+            percent = float(data_set2[0])
+            website = data_set2[1]
         else:
             data_set = data[i].split(",")
             title = data_set[0]
-        percent = float(data_set[1])
-        percent_dict[round(percent + current_total, 1)] = title
+            percent = float(data_set[1])
+            website = data_set[2]
+        percent_range = round(percent + current_total, 1)
+        occupations_dict[percent_range] = [title] # appends website as value [1] to the array associated with that occupation
+        occupations_dict[percent_range].append(website)
         current_total = current_total + percent
 
     rand_num = random.randrange(0,998) / 10
     chosen = "Chosen Occupation: "
-    for percent in percent_dict:
+    for percent in occupations_dict:
         if rand_num < percent:
-            chosen += percent_dict[percent]
+            chosen += occupations_dict[percent][0]
             break
-    return render_template('tablified.html', title = "Occupations", heading1 = heading1, heading2 = heading2, chosen_occupation = chosen, dictionary = percent_dict)
+    return render_template('tablified.html', title = "Occupations", heading1 = heading1, heading2 = heading2, chosen_occupation = chosen, dictionary = occupations_dict)
 
 if __name__ == "__main__":
     app.debug = True
