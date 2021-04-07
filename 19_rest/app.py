cb772729427db1d3c85a290a8c099156aad4ec95
Ryan Ma <ryanma223@gmail.com>
2021-04-06 23:25:27 -0400

    Add heading to files

--- 19_rest/app.py
+++ 19_rest/app.py
@@ -1,18 +1,24 @@
+#!/usr/bin/env python
+# Team cool-crabs Ryan Ma Carlos Hernandez
+# SoftDev
+# K19 -- Work with APIs and JSON
+# 2021-04-06
+
 from flask import Flask, render_template
-import urllib, json
+from urllib.request import urlopen
+from json import loads
 
 app = Flask(__name__)
 
-f = open("key_nasa.txt", "r")
-key = f.read()
-link = "https://api.nasa.gov/planetary/apod?api_key=" + key
+with open("./key_nasa.txt", "r") as f:
+    key = f.readline()
+    url = "https://api.nasa.gov/planetary/apod?api_key={}".format(key.strip())
 
-@app.route('/', methods = ["GET"])
-def display():
-    url = urllib.request.urlopen(link)
-    img = json.loads(url.read())
-    return render_template("main.html", image = img["url"])
+@app.route("/")
+def index():
+    data = loads(urlopen(url).read())
+    return render_template("main.html", pic=data["url"], expl=data["explanation"])
 
-if __name__ = '__main__':
+if __name__ == "__main__":
     app.debug = True
     app.run()
